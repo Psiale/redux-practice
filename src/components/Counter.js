6f0fe09c77a57d15e69c714f019748ecd8c32c6c
Alexis Sanchez <psialedev@gmail.com>
2021-04-12 17:50:48 -0500

    finished app with redux example

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,2 +1,22 @@
 import React from 'react-dom'
-import 
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+
+const Counter = ({counter, counterText, handleOnClick}) => (
+    <> 
+        <p>{counter}</p>
+        <button onClick={handleOnClick}>{counterText}</button>
+    </>
+)
+
+const mapStateToProps = state => ({
+    counter: state.counter,
+});
+
+Counter.propTypes = {
+    counter: PropTypes.number.isRequired,
+    counterText: PropTypes.string.isRequired,
+    handleOnClick: PropTypes.func.isRequired,
+}
+
+export default connect(mapStateToProps, null)(Counter);
